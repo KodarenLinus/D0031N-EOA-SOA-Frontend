43e31ddd1803148c3680dc98d9f41ac9b04e0776
KodarenLinus <Linus.Sideback03@gmail.com>
2025-11-06 16:10:33 +0100

    test

--- src/app/Canvas/page.tsx
+++ src/app/Canvas/page.tsx
@@ -82,7 +82,7 @@ export default function CanvasRosterToLadok() {
           onSetGrade={setGrade}
           onSetDate={setDate}
         />
-
+        {/*Button*/}
         <div className="flex items-center gap-3">
           <Button
             className="rounded-2xl px-4 py-2 shadow-sm border hover:opacity-90 disabled:opacity-50"
